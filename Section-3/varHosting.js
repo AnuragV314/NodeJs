d52e3afb0a8913702c0ea5c4b3e9ac28b2c9793a
AnuragV314 <vermagup01@gmail.com>
2023-12-01 04:43:03 +0530

    NodeJs

---
+++ Section-3/varHosting.js
@@ -0,0 +1,15 @@
+function greet(name) {
+    
+    if (name == 'Anurag') {
+        var a = 1000;
+        var msg = 'Welcome ' + name;
+    }
+    else {
+        var msg = 'Hello Guest'
+    }
+
+    console.log(msg)
+    console.log(a)
+}
+greet('Anurag')
+greet('Shanu')
